4d0707fb7be981c68555967ef52f659d5d660569
xiongchun <307916217@qq.com>
2014-06-12 18:02:08 +0800

    version5.2.1

--- data/mysql/g4studio_v5.0_mysql.sql
+++ data/mysql/g4studio_v5.0_mysql.sql
@@ -876,14 +876,14 @@ CREATE TABLE `eauser` (
 -- ----------------------------
 -- Records of eauser
 -- ----------------------------
-INSERT INTO `eauser` VALUES ('10000000', '超级用户', 'super', 'RaFutWeh0ww=', '1', '001', '0', '超级用户,享有系统最高管理权限', '3', '1');
-INSERT INTO `eauser` VALUES ('10000001', '开发人员', 'developer', 'RaFutWeh0ww=', '0', '001', '0', '1', '3', '1');
-INSERT INTO `eauser` VALUES ('10004885', '熊春_a_1', 'xiongchun_a_1', 'RaFutWeh0ww=', '0', '001002001006001', '0', '昆明市社保局管理员', '2', '1');
-INSERT INTO `eauser` VALUES ('10004886', '熊春_b_1', 'xiongchun_b_1', 'RaFutWeh0ww=', '0', '001002001006001', '0', '', '1', '1');
-INSERT INTO `eauser` VALUES ('10004887', '熊春_a_2', 'xiongchun_a_2', 'RaFutWeh0ww=', '0', '001002001006001001', '0', '五华区社保局管理员', '2', '1');
-INSERT INTO `eauser` VALUES ('10004888', '熊春_b_2', 'xiongchun_b_2', 'RaFutWeh0ww=', '0', '001002001006001001', '0', '五华区社保局业务经办员', '1', '1');
-INSERT INTO `eauser` VALUES ('10004889', '熊春_a_3', 'xiongchun_a_3', 'RaFutWeh0ww=', '0', '001002001006001002', '0', '盘龙区社保局管理员', '2', '1');
-INSERT INTO `eauser` VALUES ('10004890', '熊春_b_3', 'xiongchun_b_3', 'RaFutWeh0ww=', '0', '001002001006001002', '0', '盘龙区社保局业务经办员', '1', '1');
+INSERT INTO `eauser` VALUES ('10000000', '超级用户', 'super', 'RPEobhXzwig=', '1', '001', '0', '超级用户,享有系统最高管理权限', '3', '1');
+INSERT INTO `eauser` VALUES ('10000001', '开发人员', 'developer', 'RPEobhXzwig=', '0', '001', '0', '1', '3', '1');
+INSERT INTO `eauser` VALUES ('10004885', '熊春_a_1', 'xiongchun_a_1', 'RPEobhXzwig=', '0', '001002001006001', '0', '昆明市社保局管理员', '2', '1');
+INSERT INTO `eauser` VALUES ('10004886', '熊春_b_1', 'xiongchun_b_1', 'RPEobhXzwig=', '0', '001002001006001', '0', '', '1', '1');
+INSERT INTO `eauser` VALUES ('10004887', '熊春_a_2', 'xiongchun_a_2', 'RPEobhXzwig=', '0', '001002001006001001', '0', '五华区社保局管理员', '2', '1');
+INSERT INTO `eauser` VALUES ('10004888', '熊春_b_2', 'xiongchun_b_2', 'RPEobhXzwig=', '0', '001002001006001001', '0', '五华区社保局业务经办员', '1', '1');
+INSERT INTO `eauser` VALUES ('10004889', '熊春_a_3', 'xiongchun_a_3', 'RPEobhXzwig=', '0', '001002001006001002', '0', '盘龙区社保局管理员', '2', '1');
+INSERT INTO `eauser` VALUES ('10004890', '熊春_b_3', 'xiongchun_b_3', 'RPEobhXzwig=', '0', '001002001006001002', '0', '盘龙区社保局业务经办员', '1', '1');
 
 -- ----------------------------
 -- Table structure for `eauserauthorize`
--- data/oracle/g4studio_v5.0_oracle.sql
+++ data/oracle/g4studio_v5.0_oracle.sql
@@ -1598,21 +1598,21 @@ commit;
 prompt 15 records loaded
 prompt Loading EAUSER...
 insert into EAUSER (userid, username, account, password, sex, deptid, locked, remark, usertype, enabled)
-values ('10000000', '超级用户', 'super', 'RaFutWeh0ww=', '1', '001', '0', '超级用户,享有系统最高管理权限', '3', '1');
+values ('10000000', '超级用户', 'super', 'RPEobhXzwig=', '1', '001', '0', '超级用户,享有系统最高管理权限', '3', '1');
 insert into EAUSER (userid, username, account, password, sex, deptid, locked, remark, usertype, enabled)
-values ('10000001', '开发人员', 'developer', 'RaFutWeh0ww=', '0', '001', '0', '1', '3', '1');
+values ('10000001', '开发人员', 'developer', 'RPEobhXzwig=', '0', '001', '0', '1', '3', '1');
 insert into EAUSER (userid, username, account, password, sex, deptid, locked, remark, usertype, enabled)
-values ('10004885', '熊春_a_1', 'xiongchun_a_1', 'RaFutWeh0ww=', '0', '001002001006001', '0', '昆明市社保局管理员', '2', '1');
+values ('10004885', '熊春_a_1', 'xiongchun_a_1', 'RPEobhXzwig=', '0', '001002001006001', '0', '昆明市社保局管理员', '2', '1');
 insert into EAUSER (userid, username, account, password, sex, deptid, locked, remark, usertype, enabled)
-values ('10004886', '熊春_b_1', 'xiongchun_b_1', 'RaFutWeh0ww=', '0', '001002001006001', '0', null, '1', '1');
+values ('10004886', '熊春_b_1', 'xiongchun_b_1', 'RPEobhXzwig=', '0', '001002001006001', '0', null, '1', '1');
 insert into EAUSER (userid, username, account, password, sex, deptid, locked, remark, usertype, enabled)
-values ('10004887', '熊春_a_2', 'xiongchun_a_2', 'RaFutWeh0ww=', '0', '001002001006001001', '0', '五华区社保局管理员', '2', '1');
+values ('10004887', '熊春_a_2', 'xiongchun_a_2', 'RPEobhXzwig=', '0', '001002001006001001', '0', '五华区社保局管理员', '2', '1');
 insert into EAUSER (userid, username, account, password, sex, deptid, locked, remark, usertype, enabled)
-values ('10004888', '熊春_b_2', 'xiongchun_b_2', 'RaFutWeh0ww=', '0', '001002001006001001', '0', '五华区社保局业务经办员', '1', '1');
+values ('10004888', '熊春_b_2', 'xiongchun_b_2', 'RPEobhXzwig=', '0', '001002001006001001', '0', '五华区社保局业务经办员', '1', '1');
 insert into EAUSER (userid, username, account, password, sex, deptid, locked, remark, usertype, enabled)
-values ('10004889', '熊春_a_3', 'xiongchun_a_3', 'RaFutWeh0ww=', '0', '001002001006001002', '0', '盘龙区社保局管理员', '2', '1');
+values ('10004889', '熊春_a_3', 'xiongchun_a_3', 'RPEobhXzwig=', '0', '001002001006001002', '0', '盘龙区社保局管理员', '2', '1');
 insert into EAUSER (userid, username, account, password, sex, deptid, locked, remark, usertype, enabled)
-values ('10004890', '熊春_b_3', 'xiongchun_b_3', 'RaFutWeh0ww=', '0', '001002001006001002', '0', '盘龙区社保局业务经办员', '1', '1');
+values ('10004890', '熊春_b_3', 'xiongchun_b_3', 'RPEobhXzwig=', '0', '001002001006001002', '0', '盘龙区社保局业务经办员', '1', '1');
 commit;
 prompt 8 records loaded
 prompt Loading EAUSERAUTHORIZE...
