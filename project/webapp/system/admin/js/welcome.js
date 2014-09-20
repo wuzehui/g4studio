72a3c3f7df79f16362a88a4bd72805b87d96d399
xiongchun <307916217@qq.com>
2014-09-20 23:45:36 +0800

    bug fixed

--- project/webapp/system/admin/js/welcome.js
+++ project/webapp/system/admin/js/welcome.js
@@ -5,7 +5,7 @@
 Ext.onReady(function() {
 			new Ext.ux.TipWindow({
 						title : '<span class=commoncss>提示</span>',
-						html : '您有[0]条未读信息.',
+						html : '您有[0]条未读信息. ',
 						iconCls : 'commentsIcon'
 					}).show(Ext.getBody());
 		});
